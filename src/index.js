6fe29a4d52671656a5a35a44e1343702bee61fe3
javier cruz <eljaviercruz@outlook.com>
2019-10-04 12:37:07 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ const express = require("express");
 const rp = require("request-promise");
 const app = express();
 
+async function html(){
+    var page = await rp("https://randomuser.me/api/")
+    .then(function(html){
+        return html;
+    })
+    .catch(function(err){
+        return err;
+    })
+    console.log(page);
+}
+
 app.get("/",function(req,res){
     res.json({
-        
+        page:html()
     });
     res.end();
 });
